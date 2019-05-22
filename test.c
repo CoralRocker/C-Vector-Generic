2303b0ad5f873c741a7c0618d506e7a8e91b1fc6
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-05-22 10:24:15 -0500

    Improved test file

--- test.c
+++ test.c
@@ -19,18 +19,24 @@ void fillVector(vector *v)
 
 int main()
 {
+
+	/* Create Vector */
 	vector *v = initVector();
 	printf("Initializing vector...n");
 	for(int i = 0; i < 20; i++)
 		insertVector(v, i, i*3 + 1);
 	printVector(v);
 
+	/* Test accessor methods */
 	printf("\nAccessor Methods:\n");
 	printf("atVector(v, 5): %d\n", atVector(v, 5));
 	printf("frontVector(v): %d\n", frontVector(v));
 	printf("backVector(v): %d\n", backVector(v));
 	printf("dataVector(v): %p\n", dataVector(v));
 
+	/* Test Modifier Methods.
+	 * The methods are executed on the same line as they are printed
+	 */
 	printf("\nModifier Methods:\n");
 	printf("assignVector(v, 2, 69)\n"); assignVector(v, 2, 69);
 	printf("popBackVector(v)\n"); popBackVector(v);
@@ -40,14 +46,17 @@ int main()
 
 	printVector(v);
 	freeVector(v);
-
+	
+	/* Test Swapping vectors */
 	vector *v1 = initVector();
 	vector *v2 = initVector();
 	fillVector(v1); fillVector(v2);
-	printf("%p, %p\n", v1, v2);
-	printf("%p, %p\n", dataVector(v1), dataVector(v2));
-	printf("Swapping data pointers\n"); swapVector(v1, v2);
-	printf("%p, %p\n", dataVector(v1), dataVector(v2));
+	printf("\nVector Data Swapping:\n");
+	printf("V1: %p, V2: %p\n", v1, v2);
+	printf("V1.data: %p, V2.data: %p\n", dataVector(v1), dataVector(v2));
+	printf("Swapping data pointers...\n"); swapVector(v1, v2);	
+	printf("V1: %p, V2: %p\n", v1, v2);
+	printf("V1.data: %p, V2.data: %p\n", dataVector(v1), dataVector(v2));
 	freeVector(v1);
 	freeVector(v2);
 }	
